6f344bce86ee16f7d73b26badf6367a348fbac6e
lsqc <lsqc@lsdevcloud.net>
2025-03-27 20:53:13 +0100

    config.h: fix formatting

--- config.h
+++ config.h
@@ -81,43 +81,42 @@ static const char *dmenucmd[] = { "rofi", "-show", "drun", "-show-icons", NULL }
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_e,      spawn,          SHCMD("rofi -modi emoji -show emoji -kb-custom-1 Ctrl+c")},
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-//	{ MODKEY,                       XK_f,	   fullscreen,     {0} },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ ALTKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-//	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD("bash ~/repos/dotfiles/scripts/screenshot.sh")},
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,           	XK_l,	   spawn,	       SHCMD("betterlockscreen -l dimblur --display 1 --span")},
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	TAGKEYS(                        XK_0,                      9)
-	{ MODKEY|ShiftMask,             XK_e,      quit,           {0} },
-	{ 0,                       XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
-	{ 0,                       XF86XK_AudioMute, spawn, {.v = mutevol } },
-	{ 0,                       XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
-
+	/* modifier                     key                     function        argument */
+	{ MODKEY,                       XK_d,                   spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_e,                   spawn,          SHCMD("rofi -modi emoji -show emoji -kb-custom-1 Ctrl+c")},
+	{ MODKEY,                       XK_Return,              spawn,          {.v = termcmd } },
+//	{ MODKEY,                       XK_f,	                fullscreen,     {0} },
+	{ MODKEY,                       XK_b,                   togglebar,      {0} },
+	{ MODKEY,                       XK_j,                   focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,                   focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_h,                   setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,                   setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return,              zoom,           {0} },
+	{ ALTKEY,                       XK_Tab,                 view,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,                   killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_space,               togglefloating, {0} },
+//	{ MODKEY,                       XK_0,                   view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,                   tag,            {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_s,                   spawn,          SHCMD("bash ~/repos/dotfiles/scripts/screenshot.sh")},
+	{ MODKEY,                       XK_comma,               focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period,              focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,           	XK_l,	                spawn,	       SHCMD("betterlockscreen -l dimblur --display 1 --span")},
+	{ MODKEY|ShiftMask,             XK_comma,               tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period,              tagmon,         {.i = +1 } },
+	TAGKEYS(                        XK_1,                   0)
+	TAGKEYS(                        XK_2,                   1)
+	TAGKEYS(                        XK_3,                   2)
+	TAGKEYS(                        XK_4,                   3)
+	TAGKEYS(                        XK_5,                   4)
+	TAGKEYS(                        XK_6,                   5)
+	TAGKEYS(                        XK_7,                   6)
+	TAGKEYS(                        XK_8,                   7)
+	TAGKEYS(                        XK_9,                   8)
+	TAGKEYS(                        XK_0,                   9)
+	{ MODKEY|ShiftMask,             XK_e,                   quit,        {0} },
+	{ 0,                            XF86XK_AudioLowerVolume,spawn,      {.v = downvol } },
+	{ 0,                            XF86XK_AudioMute,       spawn,      {.v = mutevol } },
+	{ 0,                            XF86XK_AudioRaiseVolume,spawn,      {.v = upvol   } },
 };
 
 /* button definitions */
@@ -136,4 +135,3 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
